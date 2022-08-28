546619d308320a83aa5ecf24506f01aefcbac280
PeachDo <a39550897@gmail.com>
2022-08-29 01:42:09 +0900

    fix api

--- routes/deliveries/readAll.js
+++ routes/deliveries/readAll.js
@@ -2,9 +2,10 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
+    console.log(request)
     reply
-    .code(200)
-    .send ('hello world')
+        .code(200)
+        .header('Content-Type','application/json')
+        .send('test ok')
   })
 }
-
